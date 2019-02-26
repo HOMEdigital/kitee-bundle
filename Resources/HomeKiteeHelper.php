5d305609d6f3ec40dc0e6847a0a8ced2c3e05a1d
Dirk Holstein <dh@holsteinmedia.com>
2019-02-26 13:33:41 +0100

    multiCe wieder rausgenommen, da kein Sinn; es galt nur für rows und rows mit multi macht keinen Sinn die ermittlung der klassen für rows&tiles in Helper verschoben. Somit steht es auch von anderen Stellen zur Verfügung. Wird benötigt, bei Modulen, da dort htmlStart und htmlEnd nicht zur Verfügung stehen.

--- Resources/HomeKiteeHelper.php
+++ Resources/HomeKiteeHelper.php
@@ -44,4 +44,195 @@ class HomeKiteeHelper
 
         return $classes;
     }
+
+    /**
+     * return the tile_classes for row&tiles
+     *
+     * @param $article
+     */
+    public static function getRowTilesClasses($article, $itemBig = null)
+    {
+        if ($article->hm_tile_rows == 'tiles') {
+            switch ($article->hm_tile_cols) {
+                case 'hm-tiles-2cols':      return self::get2ColTileClasses($itemBig);      break;
+                case 'hm-tiles-3cols':      return self::get3ColTileClasses($itemBig);      break;
+                case 'hm-tiles-3cols-full': return self::get3ColFullTileClasses($itemBig);  break;
+                case 'hm-tiles-4cols':      return self::get4ColTileClasses($itemBig);      break;
+                case 'hm-tiles-4cols-full': return self::get4ColFullTileClasses($itemBig);  break;
+                case 'hm-tiles-5cols':      return self::get5ColTileClasses($itemBig);      break;
+                case 'hm-tiles-6cols':      return self::get6ColTileClasses($itemBig);      break;
+            }
+        } else if ($article->hm_tile_rows == 'rows') {
+            return self::getRowClasses($article->hm_rows_screensize, $article->hm_rows_size, $itemBig);
+        }
+    }
+
+    /**
+     * return the classes for the row
+     */
+    static function getRowClasses($screensize, $rowsize, $itemBig = false) {
+        #-- ermittle ab welcher Bildschirmgrösse die Spalten eingesetzt werden sollen
+        switch ($screensize) {
+            case 'layout-gt-xs-row':
+                $rowStart = 'gt-xs-';
+                break;
+            case 'layout-gt-sm-row':
+                $rowStart = 'gt-sm-';
+                break;
+            case 'layout-gt-md-row':
+                $rowStart = 'gt-md-';
+                break;
+            default:
+                $rowStart = '';
+        }
+
+        #-- wenn Spalte eine grosse Spalte ist
+        if ($itemBig) {
+            return 'flex-' . $rowStart . '100';
+        }
+
+        #-- wenn alle Spalten gleich gross
+        if ($rowsize == 'hm-rows-flex') {
+            return 'flex';
+        }
+
+        $rowSize = explode('-', $rowsize);
+
+        #-- else
+        if (count($rowSize) == 4) { /* +-- zweispaltig - hm-rows-30-70 */
+            if ($GLOBALS['kitee']['ce_number'] % 2 == 0) {
+                return 'flex-' . $rowStart . $rowSize[3];
+            } else {
+                return 'flex-' . $rowStart . $rowSize[2];
+            }
+        }
+        if (count($rowSize) > 4) { /* +-- drei- und mehrspaltig - hm-rows-33-33-33 */
+            return 'flex-' . $rowStart . $rowSize[2];
+        }
+    }
+
+    /**
+     * return classes for 2 columns tiles
+     */
+    static function get2ColTileClasses($itemBig)
+    {
+        switch ($itemBig) {
+            case "hm-tiles-1col-item" :
+                return array('flex-100');
+                break;
+            default:
+                return array('flex-xs-100', 'flex-gt-xs-50');
+        }
+    }
+
+    /**
+     * return classes for 3 columns tiles
+     */
+    static function get3ColTileClasses($itemBig)
+    {
+        switch ($itemBig) {
+            case "hm-tiles-1col-item":
+                return array('flex-100');
+                break;
+            case "hm-tiles-2col-item":
+                return array('flex-xs-100', 'flex-sm-100', 'flex-gt-sm-66');
+                break;
+            default:
+                return array('flex-xs-100', 'flex-sm-50', 'flex-gt-sm-33');
+        }
+    }
+
+    /**
+     * return classes for 3 columns full width tiles
+     */
+    static function get3ColFullTileClasses($itemBig)
+    {
+        switch ($itemBig) {
+            case "hm-tiles-1col-item":
+                return array('flex-100');
+                break;
+            case "hm-tiles-2col-item":
+                return array('flex-xs-100', 'flex-sm-100', 'flex-md-66', 'flex-lg-50', 'flex-gt-lg-40');
+                break;
+            default:
+                return array('flex-xs-100', 'flex-sm-50', 'flex-md-33', 'flex-lg-25', 'flex-gt-lg-20');
+        }
+    }
+
+    /**
+     * return classes for 4 columns tiles
+     */
+    static function get4ColTileClasses($itemBig)
+    {
+        switch ($itemBig) {
+            case "hm-tiles-1col-item":
+                return array('flex-100');
+                break;
+            case "hm-tiles-2col-item":
+                return array('flex-xs-100', 'flex-sm-66', 'flex-gt-sm-50');
+                break;
+            case "hm-tiles-3col-item":
+                return array('flex-xs-100', 'flex-sm-100', 'flex-gt-sm-75');
+                break;
+            default:
+                return array('flex-xs-50', 'flex-sm-33', 'flex-gt-sm-25');
+        }
+    }
+
+    /**
+     * return classes for 4 columns full width tiles
+     */
+    static function get4ColFullTileClasses($itemBig)
+    {
+        switch ($itemBig) {
+            case "hm-tiles-1col-item":
+                return array('flex-100');
+                break;
+            case "hm-tiles-2col-item":
+                return array('flex-xs-100', 'flex-sm-66', 'flex-md-50', 'flex-gt-md-40');
+                break;
+            case "hm-tiles-3col-item":
+                return array('flex-xs-100', 'flex-sm-100', 'flex-md-75', 'flex-gt-md-60');
+                break;
+            default:
+                return array('flex-xs-50', 'flex-sm-33', 'flex-md-25', 'flex-gt-md-20');
+        }
+    }
+
+    /**
+     * return classes for 5 columns tiles
+     */
+    static function get5ColTileClasses($itemBig)
+    {
+        switch ($itemBig) {
+            case "hm-tiles-1col-item":
+                return array('flex-100');
+                break;
+            case "hm-tiles-2col-item":
+                return array('flex-xs-100', 'flex-sm-80', 'flex-gt-sm-40');
+                break;
+            case "hm-tiles-3col-item":
+                return array('flex-xs-100', 'flex-sm-100', 'flex-gt-sm-60');
+                break;
+            default:
+                return array('flex-xs-50', 'flex-sm-33', 'flex-gt-sm-20');
+        }
+    }
+
+    /**
+     * return classes for 6 columns tiles
+     */
+    static function get6ColTileClasses($itemBig)
+    {
+        switch ($itemBig) {
+            case "hm-tiles-1col-item":
+                return array('flex-100');
+                break;
+            case "hm-tiles-2col-item":
+                return array('flex-xs-100', 'flex-sm-50', 'flex-gt-sm-25');
+                break;
+            default:
+                return array('flex-xs-33', 'flex-sm-25', 'flex-gt-sm-16');
+        }
+    }
 }
--- Resources/contao/hooks/GetContentElement.php
+++ Resources/contao/hooks/GetContentElement.php
@@ -32,10 +32,8 @@ class GetContentElement
                 if ($article->hm_tile_rows != '') {
                     $addToHtmlStart = '';
                     $addToHtmlEnd = '</div></div>';
-                    $html = "";
                     $classes = array(); // Klassen des ContentElments
                     $tile_classes = array(); // die Klassen werden dem umschliessenden Div zugewiesen
-                    $this->multiStart = null; // wenn ein multi content elment vorliegt werden hier die einzlenen start html strings gespeichert
 
                     #-- die folgenden Style-Klassen dürfen nur bei ContentElmenten hinzugefügt werden, die auf der ersten Eben stehen, also nicht
                     #-- nicht von einem Wrapper (hm_tile_container_start) umschlossen sind. Denn die Spalten- bzw. Kachel-Funktion wird nur auf die Elmente der ersten Ebene
@@ -53,53 +51,18 @@ class GetContentElement
                         #-- um die richtigen Klassen zuzuweisen
                         $GLOBALS['kitee']['ce_number'] = (in_array('first', $classes) === true) ? 1 : $GLOBALS['kitee']['ce_number'] + 1;
 
-                        #-- Zuweisen der entsprechenden Klassen, je nach dem, was im Artikel ausgewählt wurde
-                        if ($article->hm_tile_rows == 'tiles') {
-                            switch ($article->hm_tile_cols) {
-                                case 'hm-tiles-2cols':
-                                    $tile_classes = array_merge($tile_classes, self::get2ColTileClasses($objRow->__get('hm_tile_item_big')));
-                                    break;
-                                case 'hm-tiles-3cols':
-                                    $tile_classes = array_merge($tile_classes, self::get3ColTileClasses($objRow->__get('hm_tile_item_big')));
-                                    break;
-                                case 'hm-tiles-3cols-full':
-                                    $tile_classes = array_merge($tile_classes, self::get3ColFullTileClasses($objRow->__get('hm_tile_item_big')));
-                                    break;
-                                case 'hm-tiles-4cols':
-                                    $tile_classes = array_merge($tile_classes, self::get4ColTileClasses($objRow->__get('hm_tile_item_big')));
-                                    break;
-                                case 'hm-tiles-4cols-full':
-                                    $tile_classes = array_merge($tile_classes, self::get4ColFullTileClasses($objRow->__get('hm_tile_item_big')));
-                                    break;
-                                case 'hm-tiles-5cols':
-                                    $tile_classes = array_merge($tile_classes, self::get5ColTileClasses($objRow->__get('hm_tile_item_big')));
-                                    break;
-                                case 'hm-tiles-6cols':
-                                    $tile_classes = array_merge($tile_classes, self::get6ColTileClasses($objRow->__get('hm_tile_item_big')));
-                                    break;
-                            }
-                        } else if ($article->hm_tile_rows == 'tiles_isotope') {
-                            if ($objRow->__get('hm_tile_item_big')) {
-                                $classes[] = $objRow->__get('hm_tile_item_big');
-                            }
-                            $htmlSizerGutter = '<div class="hm-tiles-sizer"></div><div class="hm-tiles-gutter-sizer"></div>';
-                        } else if ($article->hm_tile_rows == 'rows') {
-                            $tile_classes[] = self::getRowClasses($article->hm_rows_screensize, $article->hm_rows_size, $objRow->__get('hm_tile_item_big'), $GLOBALS['tl_dca'][$objRow->type]['kitee']['multiCe']);
+                        #-- Zuweisen der entsprechenden row&tile Klassen, je nach dem, was im Artikel ausgewählt wurde
+                        $tile_classes = array_merge($tile_classes, HomeKiteeHelper::getRowTilesClasses($article, $objRow->__get('hm_tile_item_big')));
+
+                        if ($article->hm_tile_rows == 'tiles_isotope' && $objRow->__get('hm_tile_item_big')) {
+                            $classes[] = $objRow->__get('hm_tile_item_big');
                         }
 
                         #-- set ce additional data
                         $tileOrRowClass = ($article->hm_tile_rows == 'tiles' || $article->hm_tile_rows == 'tiles_isotope') ? 'hm-tile' : 'hm-row';
                         $tile_classes[] = $tileOrRowClass;
-
-                        if (is_array($this->multiStart)) {
-                            $multiHtmlStart = array();
-                            foreach($this->multiStart as $multiStartClass) {
-                                $multiHtmlStart[] = '<div class="' . implode(' ', $tile_classes) . ' '. $multiStartClass . '"><div class="' . $tileOrRowClass . '-inside">';
-                            }
-                            $tile_classes[] = $this->multiStart[0];
-                        }
-
                         $addToHtmlStart = '<div class="' . implode(' ', $tile_classes) . '"><div class="' . $tileOrRowClass . '-inside">';
+
 
                         #-- wenn dem Bild eine Grösse zugewiesen worden ist, dann darf es nicht mit cover (also ausfüllend) angezeigt werden
                         #-- sondern mit der angegebenen Grösse
@@ -115,9 +78,7 @@ class GetContentElement
                         $objRow->__set('classes', $classes);
                         $objRow->__set('htmlStart', $addToHtmlStart);
                         $objRow->__set('htmlEnd', $addToHtmlEnd);
-                        if (is_array($multiHtmlStart)) {
-                            $objRow->__set('multiHtmlStart', $multiHtmlStart);
-                        }
+
 
                         #-- generate a new strBuffer from objRow
                         #-- Achtung: ein neues generieren des objElement schlägt beim ContentImage fehl. Daher muss ein komplett neues objElement erzeugt werden
@@ -137,7 +98,7 @@ class GetContentElement
 
                         #-- füge notwendige divs hinzu
                         if ($classes[0] == 'first' && $article->hm_tile_rows == 'tiles_isotope') {
-                            $strBuffer = $htmlSizerGutter . $strBuffer;
+                            $strBuffer = '<div class="hm-tiles-sizer"></div><div class="hm-tiles-gutter-sizer"></div>' . $strBuffer;
                         }
 
                     } else if ($objRow->type == 'hm_content_container_end') {
@@ -152,183 +113,5 @@ class GetContentElement
         return $strBuffer;
     }
 
-    /**
-     * return the classes for the row
-     */
-    private function getRowClasses($screensize, $rowsize, $itemBig = false, $multiCe = false) {
-        #-- ermittle ab welcher Bildschirmgrösse die Spalten eingesetzt werden sollen
-        switch ($screensize) {
-            case 'layout-gt-xs-row':
-                $rowStart = 'gt-xs-';
-                break;
-            case 'layout-gt-sm-row':
-                $rowStart = 'gt-sm-';
-                break;
-            case 'layout-gt-md-row':
-                $rowStart = 'gt-md-';
-                break;
-            default:
-                $rowStart = '';
-        }
 
-        #-- wenn Spalte eine grosse Spalte ist
-        if ($itemBig) {
-            return 'flex-' . $rowStart . '100';
-        }
-
-        #-- wenn alle Spalten gleich gross
-        if ($rowsize == 'hm-rows-flex') {
-            return 'flex';
-        }
-
-        $rowSize = explode('-', $rowsize);
-
-        #-- wenn es sich um ein multi content element handelt -> wenn das CE mehrere content elemente ausgibt
-        if ($multiCe === true) {
-            $this->multiStart = array();
-            foreach($rowSize as $key=>$val) {
-                if ($key > 1) {
-                    $this->multiStart[] = 'flex-' . $rowStart . $rowSize[$key];
-                }
-            }
-            return '';
-        }
-
-        #-- else
-        if (count($rowSize) == 4) { /* +-- zweispaltig - hm-rows-30-70 */
-            if ($GLOBALS['kitee']['ce_number'] % 2 == 0) {
-                return 'flex-' . $rowStart . $rowSize[3];
-            } else {
-                return 'flex-' . $rowStart . $rowSize[2];
-            }
-        }
-        if (count($rowSize) > 4) { /* +-- drei- und mehrspaltig - hm-rows-30-70 */
-            return 'flex-' . $rowStart . $rowSize[2];
-        }
-    }
-
-    /**
-     * return classes for 2 columns tiles
-     */
-    private function get2ColTileClasses($itemBig)
-    {
-        switch ($itemBig) {
-            case "hm-tiles-1col-item" :
-                return array('flex-100');
-                break;
-            default:
-                return array('flex-xs-100', 'flex-gt-xs-50');
-        }
-    }
-
-    /**
-     * return classes for 3 columns tiles
-     */
-    private function get3ColTileClasses($itemBig)
-    {
-        switch ($itemBig) {
-            case "hm-tiles-1col-item":
-                return array('flex-100');
-                break;
-            case "hm-tiles-2col-item":
-                return array('flex-xs-100', 'flex-sm-100', 'flex-gt-sm-66');
-                break;
-            default:
-                return array('flex-xs-100', 'flex-sm-50', 'flex-gt-sm-33');
-        }
-    }
-
-    /**
-     * return classes for 3 columns full width tiles
-     */
-    private function get3ColFullTileClasses($itemBig)
-    {
-        switch ($itemBig) {
-            case "hm-tiles-1col-item":
-                return array('flex-100');
-                break;
-            case "hm-tiles-2col-item":
-                return array('flex-xs-100', 'flex-sm-100', 'flex-md-66', 'flex-lg-50', 'flex-gt-lg-40');
-                break;
-            default:
-                return array('flex-xs-100', 'flex-sm-50', 'flex-md-33', 'flex-lg-25', 'flex-gt-lg-20');
-        }
-    }
-
-    /**
-     * return classes for 4 columns tiles
-     */
-    private function get4ColTileClasses($itemBig)
-    {
-        switch ($itemBig) {
-            case "hm-tiles-1col-item":
-                return array('flex-100');
-                break;
-            case "hm-tiles-2col-item":
-                return array('flex-xs-100', 'flex-sm-66', 'flex-gt-sm-50');
-                break;
-            case "hm-tiles-3col-item":
-                return array('flex-xs-100', 'flex-sm-100', 'flex-gt-sm-75');
-                break;
-            default:
-                return array('flex-xs-50', 'flex-sm-33', 'flex-gt-sm-25');
-        }
-    }
-
-    /**
-     * return classes for 4 columns full width tiles
-     */
-    private function get4ColFullTileClasses($itemBig)
-    {
-        switch ($itemBig) {
-            case "hm-tiles-1col-item":
-                return array('flex-100');
-                break;
-            case "hm-tiles-2col-item":
-                return array('flex-xs-100', 'flex-sm-66', 'flex-md-50', 'flex-gt-md-40');
-                break;
-            case "hm-tiles-3col-item":
-                return array('flex-xs-100', 'flex-sm-100', 'flex-md-75', 'flex-gt-md-60');
-                break;
-            default:
-                return array('flex-xs-50', 'flex-sm-33', 'flex-md-25', 'flex-gt-md-20');
-        }
-    }
-
-    /**
-     * return classes for 5 columns tiles
-     */
-    private function get5ColTileClasses($itemBig)
-    {
-        switch ($itemBig) {
-            case "hm-tiles-1col-item":
-                return array('flex-100');
-                break;
-            case "hm-tiles-2col-item":
-                return array('flex-xs-100', 'flex-sm-80', 'flex-gt-sm-40');
-                break;
-            case "hm-tiles-3col-item":
-                return array('flex-xs-100', 'flex-sm-100', 'flex-gt-sm-60');
-                break;
-            default:
-                return array('flex-xs-50', 'flex-sm-33', 'flex-gt-sm-20');
-        }
-    }
-
-    /**
-     * return classes for 6 columns tiles
-     */
-    private function get6ColTileClasses($itemBig)
-    {
-        switch ($itemBig) {
-            case "hm-tiles-1col-item":
-                return array('flex-100');
-                break;
-            case "hm-tiles-2col-item":
-                return array('flex-xs-100', 'flex-sm-50', 'flex-gt-sm-25');
-                break;
-            default:
-                return array('flex-xs-33', 'flex-sm-25', 'flex-gt-sm-16');
-        }
-    }
 }
