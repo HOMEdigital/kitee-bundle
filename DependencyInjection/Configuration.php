aabd64de0e7ded3603c70749404a641fa21e6351
HOMEdigital <41010376+HOMEdigital@users.noreply.github.com>
2022-04-22 10:56:56 +0200

    Update Symfony TreeBuilder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('home_kitee');
+        $treeBuilder = new TreeBuilder('home_kitee');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
