94d6ad5c4b762af55129a67b5e86869bff5b1606
dirk.holstein <holstein@home.digital>
2022-08-31 17:49:06 +0200

    Bug mit array merge in ContentContainerStart

--- Resources/contao/elements/ContainerContentStartElement.php
+++ Resources/contao/elements/ContainerContentStartElement.php
@@ -46,9 +46,16 @@ class ContainerContentStartElement extends \ContentElement
     {
         #-- add classes
 
-        $this->objModel->classes = array_unique(array_merge($this->objModel->classes, HomeKiteeHelper::getLayoutClasses(array(
-            'design' => $this->hm_design
-        ))));
+        if (is_array($this->objModel->classes)) {
+            $this->objModel->classes = array_unique(array_merge($this->objModel->classes, HomeKiteeHelper::getLayoutClasses(array(
+                'design' => $this->hm_design
+            ))));
+        } else {
+            $this->objModel->classes = array_unique(HomeKiteeHelper::getLayoutClasses(array(
+                'design' => $this->hm_design
+            )));
+
+        }
 
         #-- store the wrapper start; will be closed in end element
         $GLOBALS['kitee']['container'] = 'containerTile';
